11bf4f8908df102f20abcb839f5b17761e0bcf02
Koushik Sharma <1974kamalsharma@gmail.com>
2026-04-06 20:31:09 +0530

    added a corousel behind login box

--- Movie-Booking/src/Nav.jsx
+++ Movie-Booking/src/Nav.jsx
@@ -6,14 +6,14 @@ export default function Nav({collapsed, setCollapsed,}) {
     return(
 
         <div className={`nav ${collapsed ? 'collapsed' : ''}`}>
-            <button className="nav-toggle" onClick = {() =>setCollapsed(!collapsed)} ><img src = "nav-bar-icons/toggle-for-nav.png" /></button>
+            <button className="nav-toggle" onClick = {() =>setCollapsed(!collapsed)} ><img src = "src/assets/nav-bar-icons/toggle-for-nav.png" /></button>
             <ul className="nav-list">
                 
-                <li><NavLink to ="/login" className = {({isActive}) => isActive ? "active" : ""}><img src="nav-bar-icons/login.png" />{!collapsed && "Login"}</NavLink></li>
-                <li><NavLink to ="/home" className = {({isActive}) => isActive ? "active" : ""} ><img src="nav-bar-icons/home.png" />{!collapsed && "Home"}</NavLink></li>
-                <li><NavLink to ="/browse" className = {({isActive}) => isActive ? "active" : ""} ><img src="nav-bar-icons/browse.png" />{!collapsed && "Browse"}</NavLink></li>
-                <li><NavLink to ="/bookings" className = {({isActive}) => isActive ? "active" : ""} ><img src="nav-bar-icons/bookings.png" />{!collapsed && "Bookings"}</NavLink></li>
-                <li><NavLink to ="/snacks" className = {({isActive}) => isActive ? "active" : ""} ><img src="nav-bar-icons/snacks.png" />{!collapsed && "Snacks"}</NavLink></li>
+                <li><NavLink to ="/login" className = {({isActive}) => isActive ? "active" : ""}><img src="src/assets/nav-bar-icons/login.png" />{!collapsed && "Login"}</NavLink></li>
+                <li><NavLink to ="/home" className = {({isActive}) => isActive ? "active" : ""} ><img src="src/assets/nav-bar-icons/home.png" />{!collapsed && "Home"}</NavLink></li>
+                <li><NavLink to ="/browse" className = {({isActive}) => isActive ? "active" : ""} ><img src="src/assets/nav-bar-icons/browse.png" />{!collapsed && "Browse"}</NavLink></li>
+                <li><NavLink to ="/bookings" className = {({isActive}) => isActive ? "active" : ""} ><img src="src/assets/nav-bar-icons/bookings.png" />{!collapsed && "Bookings"}</NavLink></li>
+                <li><NavLink to ="/snacks" className = {({isActive}) => isActive ? "active" : ""} ><img src="src/assets/nav-bar-icons/snacks.png" />{!collapsed && "Snacks"}</NavLink></li>
             </ul>
          
         </div>
--- Movie-Booking/src/login.jsx
+++ Movie-Booking/src/login.jsx
@@ -2,6 +2,29 @@ import "./login.css"
 export default function Login() {
     return(
         <div className="login-content">
+            <div className = "bg-corousel">
+                <div className="row row1">
+                    <img className="img1" src="/src/assets/login-corousel/p1.jpg"></img>
+                    <img className="img2" src="/src/assets/login-corousel/p2.jpg"></img>
+                    <img className="img3" src="/src/assets/login-corousel/p3.jpg"></img>
+                    <img className="img4" src="/src/assets/login-corousel/p1.jpg"></img>
+                    <img className="img5" src="/src/assets/login-corousel/p2.jpg"></img>
+                </div>
+                <div className="row row2">
+                    <img className="img6" src="/src/assets/login-corousel/p4.jpg"></img>
+                    <img className="img7" src="/src/assets/login-corousel/p5.jpg"></img>
+                    <img className="img8" src="/src/assets/login-corousel/p6.jpg"></img>
+                    <img className="img9" src="/src/assets/login-corousel/p4.jpg"></img>
+                    <img className="img10" src="/src/assets/login-corousel/p5.jpg"></img>
+                </div>
+                <div className="row row3">
+                    <img className="img11" src="/src/assets/login-corousel/p7.jpg"></img>
+                    <img className="img12" src="/src/assets/login-corousel/p8.jpg"></img>
+                    <img className="img13" src="/src/assets/login-corousel/p9.jpg"></img>
+                    <img className="img14" src="/src/assets/login-corousel/p7.jpg"></img>
+                    <img className="img15" src="/src/assets/login-corousel/p8.jpg"></img>
+                </div>
+            </div>
             <div className="login-box">
                 <h2>Login</h2>
                 <form>
